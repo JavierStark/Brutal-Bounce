f7ff5acb90b9b07f2a05c0444b79f3511b38b075
JavierStark <JavierTorralboCortes@gmail.com>
2020-10-15 20:36:25 +0200

    Prototipo en vertical

--- Brutal Bounce/Assets/Scripts/Ball.cs
+++ Brutal Bounce/Assets/Scripts/Ball.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    Rigidbody2D rigidbody;
+    float velocity = 10;
+    int noPlayerBounce = 0;
+    
+
+    private void Awake() {
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Start() {
+        rigidbody.AddForce(new Vector2(0, -1));
+    }
+    private void LateUpdate() {
+        rigidbody.velocity = velocity * (rigidbody.velocity.normalized);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
         
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("UpWall")) {
+            
+            noPlayerBounce = 0;
+            rigidbody.gravityScale = 0;
+            float randomX = (Random.Range(0,2) == 0) ? 300 : -300;
+            rigidbody.AddForce(-collision.contacts[0].normal +
+                    new Vector2(randomX,0));
+
+        }
+        else if(collision.gameObject.CompareTag("SideWall")){
+            noPlayerBounce++;
+            //float randomY = (Random.Range(0,2) == 0) ? 400 : -400;
+            //rigidbody.AddForce(-collision.contacts[0].normal +
+            //        new Vector2(0, -300));
+
+            if (noPlayerBounce > 4) {
+                rigidbody.gravityScale = 1;
+                noPlayerBounce = 0;                
+            }            
+        }
     }
 }
---
+++ Brutal Bounce/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    SpriteRenderer sprite;
+
+    float horizontalInput;
+    [SerializeField] float velocity = 1;
+
+    private void Awake() {
+        rigidbody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update(){
+        GetInput();
+    }
+    private void FixedUpdate() {
+        Vector2 rbVel = new Vector2(horizontalInput, 0)*velocity;
+        rigidbody.velocity = rbVel;
+    }
+
+    void GetInput() {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+}

626bed696c63f5ac23cfcc194234f2b0484eb7bf
JavierStark <JavierTorralboCortes@gmail.com>
2020-10-15 21:03:16 +0200

    Proto final, con gravedad

--- Brutal Bounce/Assets/Scripts/Ball.cs
+++ Brutal Bounce/Assets/Scripts/Ball.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     Rigidbody2D rigidbody;
-    float velocity = 10;
+    float velocity = 15;
     int noPlayerBounce = 0;
     
 
@@ -13,11 +13,8 @@ public class Ball : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    private void Start() {
-        rigidbody.AddForce(new Vector2(0, -1));
-    }
     private void LateUpdate() {
-        rigidbody.velocity = velocity * (rigidbody.velocity.normalized);
+        rigidbody.velocity = velocity * rigidbody.velocity.normalized;
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
@@ -25,22 +22,11 @@ public class Ball : MonoBehaviour
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("UpWall")) {
             
             noPlayerBounce = 0;
-            rigidbody.gravityScale = 0;
-            float randomX = (Random.Range(0,2) == 0) ? 300 : -300;
+            float randomX = Random.Range(-500f,500f);
             rigidbody.AddForce(-collision.contacts[0].normal +
                     new Vector2(randomX,0));
 
         }
-        else if(collision.gameObject.CompareTag("SideWall")){
-            noPlayerBounce++;
-            //float randomY = (Random.Range(0,2) == 0) ? 400 : -400;
-            //rigidbody.AddForce(-collision.contacts[0].normal +
-            //        new Vector2(0, -300));
 
-            if (noPlayerBounce > 4) {
-                rigidbody.gravityScale = 1;
-                noPlayerBounce = 0;                
-            }            
-        }
     }
 }

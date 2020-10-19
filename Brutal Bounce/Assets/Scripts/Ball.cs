db66c00143cd3c7787ea2bf4799a0fde5b1fd8c0
JavierStark <JavierTorralboCortes@gmail.com>
2020-10-19 18:19:47 +0200

    Prueba con vs code

--- Brutal Bounce/Assets/Scripts/Ball.cs
+++ Brutal Bounce/Assets/Scripts/Ball.cs
@@ -11,39 +11,45 @@ public class Ball : MonoBehaviour
     int noPlayerBounce = 0;
 
     ScoreManager scoreManager;
-    
 
-    private void Awake() {
+
+    private void Awake()
+    {
         rigidbody = GetComponent<Rigidbody2D>();
         scoreManager = FindObjectOfType<ScoreManager>();
     }
 
-    private void LateUpdate() {
+    private void LateUpdate()
+    {
         rigidbody.velocity = velocity * rigidbody.velocity.normalized;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("UpWall")) {
-            
-            noPlayerBounce = 0;
-            float randomX = Random.Range(-500f,500f);
-            rigidbody.AddForce(-collision.contacts[0].normal +
-                    new Vector2(randomX,0));
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("UpWall"))
+        {
+            noPlayerBounce = 0;
+            float randomX = Random.Range(-500f, 500f);
+            rigidbody.AddForce(-collision.contacts[0].normal +
+                    new Vector2(randomX, 0));
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.CompareTag("GameOverCollider")) {            
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("GameOverCollider"))
+        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-        else if (collision.gameObject.CompareTag("Coin")) {
+        else if (collision.gameObject.CompareTag("Coin"))
+        {
             InteractionWithCoin(collision.gameObject.GetComponent<Coin>());
         }
     }
 
-    private void InteractionWithCoin(Coin coin) {
+    private void InteractionWithCoin(Coin coin)
+    {
         scoreManager.AddScore(coin.GetValue());
         Destroy(coin.gameObject);
     }

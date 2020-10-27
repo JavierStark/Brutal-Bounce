5574caa4adbe0f79deecbd68d3f1b6732d3e5086
JavierStark <JavierTorralboCortes@gmail.com>
2020-10-27 17:27:14 +0100

    Revert "Revert "Tienda full mejorada con selección y preview""
    This reverts commit 8393a90a60bf38d1c534083598f63436e259e67c.

--- Brutal Bounce/Assets/Scripts/Shop/IItem.cs
+++ Brutal Bounce/Assets/Scripts/Shop/IItem.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 public interface IItem
 {
-    bool Bought { get; set; }
     int Price { get; set; }
     Sprite ShopImage { get; set; }
 }
--- Brutal Bounce/Assets/Scripts/Shop/ItemButton.cs
+++ Brutal Bounce/Assets/Scripts/Shop/ItemButton.cs
@@ -6,6 +6,79 @@ using TMPro;
 
 public class ItemButton : MonoBehaviour
 {
-    public Image previewImage;
-    public TMP_Text priceText;
+    [SerializeField] GameObject notBoughtPanel;
+    [SerializeField] Image previewImage;
+    [SerializeField] TMP_Text priceText;
+    [SerializeField] GameObject check;
+    private bool bought;
+    private int index;
+    private BuyButtomHandler handler;
+
+    private IItem item;
+
+    public void SetButton(Sprite sprite, string text, bool bought, int index, BuyButtomHandler handler, IItem item)
+    {
+        previewImage.sprite = sprite;
+        priceText.text = text;
+        this.bought = bought;
+        this.index = index;
+        this.handler = handler;
+        this.item = item;
+        handler.OnSkinSelected += DeselectSkin;
+        if (!bought)
+        {
+            notBoughtPanel.SetActive(true);
+        }
+        else
+        {
+            notBoughtPanel.SetActive(false);
+            CheckSelection();
+        }
+
+
+    }
+
+    public void Buy()
+    {
+        if (!bought)
+        {
+            bought = true;
+            handler.Buy(index);
+            notBoughtPanel.SetActive(false);
+        }
+        else
+        {
+            SelectSkin();
+        }
+
+    }
+
+    private void SelectSkin()
+    {
+        handler.SelectSkin(index);
+    }
+
+    private void DeselectSkin(int x)
+    {
+        if (x != index)
+        {
+            check.SetActive(false);
+        }
+        else
+        {
+            check.SetActive(true);
+        }
+    }
+
+    private void CheckSelection()
+    {
+        if (handler.GetInfo() == item)
+        {
+            check.SetActive(true);
+        }
+        else
+        {
+            check.SetActive(false);
+        }
+    }
 }

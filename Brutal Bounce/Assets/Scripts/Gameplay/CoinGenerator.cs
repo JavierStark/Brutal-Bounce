9d5f3a28c5ce012ccb825cf186e752f21620ff1f
JavierStark <JavierTorralboCortes@gmail.com>
2020-10-28 20:27:08 +0100

    Escalado de paredes y spawn seguro de props

--- Brutal Bounce/Assets/Scripts/Gameplay/CoinGenerator.cs
+++ Brutal Bounce/Assets/Scripts/Gameplay/CoinGenerator.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class CoinGenerator : MonoBehaviour
 {
-    const float minWidth = -9;
-    const float maxWidth = 9;
-    const float minHeight = -4;
-    const float maxHeight = 9;
+    float minWidth = -9;
+    float maxWidth = 9;
+    float minHeight = -4;
+    float maxHeight = 9;
+
+    Vector2 screenInUnits;
 
     [SerializeField] GameObject coin;
     [SerializeField] int coinsInGame = 1;
 
-
+    void Start()
+    {
+        screenInUnits = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.childCount < coinsInGame) {
-            Vector2 newCoinPos = new Vector2(Random.Range(minWidth, maxWidth), Random.Range(minHeight, maxHeight));
+        if (transform.childCount < coinsInGame)
+        {
+            Vector2 newCoinPos = new Vector2(Random.Range(-screenInUnits.x, screenInUnits.x), Random.Range(-screenInUnits.y + (screenInUnits.y / 2), screenInUnits.y));
             Instantiate(coin, newCoinPos, Quaternion.identity, transform);
         }
     }
---
+++ Brutal Bounce/Assets/Scripts/Gameplay/Wall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    BoxCollider2D collider2D;
+    RectTransform rectTransform;
+
+    void Start()
+    {
+        collider2D = GetComponent<BoxCollider2D>();
+        rectTransform = GetComponent<RectTransform>();
+
+        SetColliderSize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetColliderSize()
+    {
+        collider2D.size = new Vector2(rectTransform.rect.width, rectTransform.rect.height);
+    }
+}

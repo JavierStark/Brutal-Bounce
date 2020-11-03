0a4c5b0eda973d15b42a30a70f07e7ac29d8c570
Dominio Games <dominiogames.es@gmail.com>
2020-11-02 19:16:34 -0600

    Interfaz corregida 35%

---
+++ Brutal Bounce/Assets/Assets - RS/Scripts/SpecialEvents.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Events", menuName = "Brutal Bounce/Evets", order = 2)]
+public class SpecialEvents : ScriptableObject {
+
+    public bool halloWeen, christmas;
+  
+}

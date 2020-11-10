f5caf3fe5330e1a73b7107616cb1a89e44a9ae6b
JavierStark <JavierTorralboCortes@gmail.com>
2020-11-10 19:27:47 +0100

    Ranking ajustable

--- Brutal Bounce/Assets/Assets - RS/UI/Scripts/RankPanel.cs
+++ Brutal Bounce/Assets/Assets - RS/UI/Scripts/RankPanel.cs
@@ -7,10 +7,11 @@ public class RankPanel : MonoBehaviour
     public Animator panelRank;
     public bool activP;
 
-
+    [SerializeField] LeaderboardManager leaderboardManager;
 
     public void Interact()
     {
+        leaderboardManager.ShowLeaderboard();
         activP = !activP;
         panelRank.SetBool("Activ", activP);
     }
--- Brutal Bounce/Assets/Scripts/PlayFab/LeaderboardManager.cs
+++ Brutal Bounce/Assets/Scripts/PlayFab/LeaderboardManager.cs
@@ -1,8 +1,36 @@
 using UnityEngine;
 using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
 
 public class LeaderboardManager : MonoBehaviour
 {
     [SerializeField] GameObject leaderboardEntity;
+    bool opened = false;
 
+    VerticalLayoutGroup layoutGroup;
+    RectTransform rect;
+    void Awake()
+    {
+        layoutGroup = GetComponent<VerticalLayoutGroup>();
+        rect = GetComponent<RectTransform>();
+    }
+
+    public void ShowLeaderboard()
+    {
+        var request = new GetLeaderboardRequest { StatisticName = "Score", MaxResultsCount = 10 };
+        PlayFabClientAPI.GetLeaderboard(request, ShowLeaderboardSuccess, error => { });
+    }
+
+    private void ShowLeaderboardSuccess(GetLeaderboardResult result)
+    {
+        if (opened) return;
+        var leaderboard = result.Leaderboard;
+        foreach (PlayerLeaderboardEntry entry in leaderboard)
+        {
+            var currentLeaderboardEntity = Instantiate(leaderboardEntity, transform);
+            currentLeaderboardEntity.GetComponent<LeaderboardEntity>().SetInformation(entry.Position + 1, entry.DisplayName, entry.StatValue);
+        }
+        opened = true;
+    }
 }

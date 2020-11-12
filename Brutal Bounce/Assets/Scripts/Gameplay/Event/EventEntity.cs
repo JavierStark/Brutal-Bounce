7c3cd6db436259b03c1c283dddd1c7a2b0f1085a
JavierStark <JavierTorralboCortes@gmail.com>
2020-11-12 18:31:52 +0100

    Sistema de spawn de vecinos y mejora en el sistema global de eventos

---
+++ Brutal Bounce/Assets/Scripts/Gameplay/Event/EventEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventEntity : MonoBehaviour
+{
+    protected Event handlerEvent;
+    public void SetToHandlerEvent(Event e)
+    {
+        handlerEvent = e;
+    }
+}
---
+++ Brutal Bounce/Assets/Scripts/Gameplay/Event/NeigboorhoodEvent/Neighbor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neighbor : EventEntity
+{
+    void OnDestroy()
+    {
+        handlerEvent.RoundEnded();
+    }
+}

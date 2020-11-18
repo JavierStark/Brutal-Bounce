d7dbd84c663e39661bfb0436365659d60af731a5
Dominio Games <dominiogames.es@gmail.com>
2020-11-18 09:42:51 -0600

    Creditos Actualizados

---
+++ Brutal Bounce/Assets/Assets - RS/Trailers, Teasers/IMG/Scripts/BrBroTrailer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BrBroTrailer : MonoBehaviour
+{
+    public int limit = 3;
+    public int actu;
+    public Animator animator;
+
+    public TMP_Text text;
+
+    public string[] info;
+
+
+
+    public void Change(){
+      
+        actu += 1;
+        text.text = info[actu].ToString();
+
+        if(actu > limit)
+        {
+                animator.SetTrigger("End");
+        }
+
+    }
+    public void End(){
+        
+    }
+}

752d2588e932638dd6f1d03de4cb75f9c5d88b72
Dominio Games <dominiogames.es@gmail.com>
2020-11-25 08:46:56 -0600

    Isotipo cambiado

---
+++ Brutal Bounce/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public SpriteRenderer building;
+
+    void Start()
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = building.bounds.size.x / building.bounds.size.y;
+
+        if(screenRatio >= targetRatio){
+            Camera.main.orthographicSize = building.bounds.size.y / 2;
+        }else{
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = building.bounds.size.y / 2 * differenceInSize;
+        }
+    }
+
+}

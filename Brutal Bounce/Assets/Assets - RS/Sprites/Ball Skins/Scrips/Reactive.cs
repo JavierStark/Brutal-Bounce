f684cf2531ce54866869dcc6564ef078ef9c277b
Dominio Games <dominiogames.es@gmail.com>
2020-11-28 10:12:55 -0600

    Nueva tienda y transisiones

---
+++ Brutal Bounce/Assets/Assets - RS/Sprites/Ball Skins/Scrips/Reactive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reactive : MonoBehaviour
+{
+    public Animator animator;
+    public AudioSource audio;
+    public AudioClip[] effects;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        int rand = Random.Range(0, effects.Length);
+        audio.clip = (effects[rand]);   
+        
+        if(other.gameObject.tag == "Wall")
+        {
+            animator.SetTrigger("Golpe");
+            audio.Play();
+        }
+    }
+}

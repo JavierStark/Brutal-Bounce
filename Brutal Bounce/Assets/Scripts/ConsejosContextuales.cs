1fa458bb623e6c3373297577ffbcd81a78102dfa
Dominio Games <dominiogames.es@gmail.com>
2020-12-21 10:46:14 -0600

    Consejos agregados.

---
+++ Brutal Bounce/Assets/Scripts/ConsejosContextuales.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ConsejosContextuales : MonoBehaviour
+{
+
+    public TMP_Text textC;
+
+    [TextArea(1, 5)]
+    public string[] consejos;
+
+    private void Awake() {
+
+        int rand = Random.Range(0, consejos.Length);
+        textC.text = consejos[rand].ToString();
+    }
+}

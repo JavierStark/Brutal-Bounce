5e3f4f8da65060fd7902be14443e8f8c4da7a868
Recino Studio <dominiogaming@gmail.com>
2021-02-11 08:09:52 -0600

    Cambios v0.3

---
+++ Brutal Bounce/Assets/Assets - RS/Trailers, Teasers/IMG/Trailer/Scritp/Panels_Text.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panels_Text: MonoBehaviour
+{
+    public GameObject current;
+    public GameObject next;
+
+    public void Call()
+    {
+        Invoke("Change", 2f);
+    }
+
+    void Change()
+    {
+        current.SetActive(false);
+        next.SetActive(true);
+    }
+}

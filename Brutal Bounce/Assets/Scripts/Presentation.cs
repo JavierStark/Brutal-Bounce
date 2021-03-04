699d53548ba4b3abc2e594e23eaa8d85933da1ed
JavierStark <JavierTorralboCortes@gmail.com>
2021-03-04 19:38:23 +0100

    Consejos aleatorios y comienzos de la presentación

---
+++ Brutal Bounce/Assets/Scripts/Presentation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Presentation : MonoBehaviour
+{
+    Animator girlAnimator;
+
+    GameObject dialogPanel;
+    TMPro.TMP_Text dialogText;
+
+    void Awake()
+    {
+        girlAnimator = GetComponentInChildren<Animator>();
+        dialogPanel = transform.GetChild(1).gameObject;
+        dialogText = dialogPanel.GetComponentInChildren<TMPro.TMP_Text>();
+    }
+
+    public void NextPart()
+    {
+        girlAnimator.SetTrigger("Next");
+    }
+}

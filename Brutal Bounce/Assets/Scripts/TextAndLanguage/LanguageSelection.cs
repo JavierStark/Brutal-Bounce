64b48e2f86f20deaf5b48ae6aa0b33e83491b288
JavierStark <javiertorralbocortes@gmail.com>
2021-03-21 17:16:33 +0100

    Selección de lenguaje

---
+++ Brutal Bounce/Assets/Scripts/TextAndLanguage/LanguageSelection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageSelection : MonoBehaviour
+{
+    public void SetLanguage(int n){
+        PlayerPrefs.SetInt("Language", n);
+    }
+}
